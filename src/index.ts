c2af35fc6ec10a5a4d3034b439a92fc779bd2a9f
Meryl Hu <mhuconcern@oneconcern.com>
2020-04-16 15:03:13 -0700

    Update to latest

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-import * as MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
-import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import * as mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './styles.scss';
 
-(mapboxgl as any).accessToken = 'pk.eyJ1Ijoic2FnZXdhbGwiLCJhIjoiY2oyMmpla213MDBmdDMybWtjY3lyYzRjeSJ9.Nd7bpm84x9CerBQz9DG_Cw';
+(mapboxgl as any).accessToken = 'pk.eyJ1Ijoibmljb2xlaHUiLCJhIjoiSUFHaHEyVSJ9.sQh47fHih8dvNgItm92dqg';
 
 const map = new mapboxgl.Map({
     container: 'map',
@@ -12,8 +10,3 @@ const map = new mapboxgl.Map({
     center: [-105.185583, 39.742447],
     zoom: 10
 });
-
-map.addControl(new MapboxGeocoder({
-    accessToken: mapboxgl.accessToken,
-    mapboxgl: mapboxgl
-}))
