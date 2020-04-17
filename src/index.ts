eff0ad949c7f79d925e8394b4da1adf14ab91be9
Meryl Hu <mhuconcern@oneconcern.com>
2020-04-16 19:29:33 -0700

    Working version

--- src/index.ts
+++ src/index.ts
@@ -5,49 +5,58 @@ import Windy from './windy';
 const data = require('./gfs.json');
 
 (mapboxgl as any).accessToken = 'pk.eyJ1Ijoibmljb2xlaHUiLCJhIjoiSUFHaHEyVSJ9.sQh47fHih8dvNgItm92dqg';
-
 const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [95.899147, 18.088694],
-    zoom: 10
+    style: 'mapbox://styles/mapbox/dark-v10',
+    center: [-99.076, 39.132],
+    zoom: 2
 });
 
 (window as any).Map = map;
 
 map.on("load", function () {
-  map.addSource("canvas-source", {
-    type: "canvas",
-    canvas: "canvasID",
-    coordinates: [
-      [91.4461, 21.5006],
-      [100.3541, 21.5006],
-      [100.3541, 13.9706],
-      [91.4461, 13.9706],
-    ],
-    // Set to true if the canvas source is animated. If the canvas is static, animate should be set to false to improve performance.
-    animate: true,
+    var bounds = new mapboxgl.LngLatBounds([42.51529678358733,1.4153098311803234,77.37276507109567,42.10251144607963]);
+    map.addSource("canvas-source", {
+      type: "canvas",
+      canvas: "canvasID",
+      coordinates: [
+        bounds.getNorthEast().toArray(),
+        bounds.getNorthWest().toArray(),
+        bounds.getSouthWest().toArray(),
+        bounds.getSouthEast().toArray()
+      ],
+      // Set to true if the canvas source is animated. If the canvas is static, animate should be set to false to improve performance.
+      animate: true,
+    });
+  
+    map.addLayer({
+      id: "canvas-layer",
+      type: "raster",
+      source: "canvas-source",
+    });
+    redraw();
   });
-
-  map.addLayer({
-    id: "canvas-layer",
-    type: "raster",
-    source: "canvas-source",
-  });
-});
-var canvas = document.getElementById('canvasID');
-var windy = Windy({ canvas, data });
-redraw();
-function redraw(){
+  var canvas : HTMLCanvasElement = document.getElementById('canvasID') as HTMLCanvasElement;
+  var windy = Windy({ canvas, data });
+  map.on('resize', redraw);
+  function redraw(){
+    const canvasMap: mapboxgl.CanvasSource = map.getSource('canvas-source') as mapboxgl.CanvasSource;
+    const bounds = map.getBounds();
+    canvasMap.setCoordinates([
+      bounds.getNorthWest().toArray(),
+      bounds.getNorthEast().toArray(),
+      bounds.getSouthEast().toArray(),
+      bounds.getSouthWest().toArray()
+    ])
+    canvasMap.getCanvas().height = map.getCanvas().height;
+    canvasMap.getCanvas().width = map.getCanvas().width;
     windy.stop();
-
-    var extent = map.getBounds();
     setTimeout(function(){
       windy.start(
         [[0,0],[map.getCanvas().width, map.getCanvas().height]],
         map.getCanvas().width,
         map.getCanvas().height,
-        extent.toArray(),
+        bounds.toArray(),
       );
     },500);
-  }
+    } 
