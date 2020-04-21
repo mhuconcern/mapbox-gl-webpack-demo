3a17e6df87b6cec9b1dc5ae18d4023af5f9c5375
Meryl Hu <mhuconcern@oneconcern.com>
2020-04-21 10:26:23 -0700

    Modify windy for lower res

--- src/windy.js
+++ src/windy.js
@@ -12,11 +12,11 @@
 
 var Windy = function( params ){
   var VELOCITY_SCALE = 0.011;             // scale for wind velocity (completely arbitrary--this value looks nice)
-  var INTENSITY_SCALE_STEP = 10;            // step size of particle intensity color scale
+  var INTENSITY_SCALE_STEP = 1.5;            // step size of particle intensity color scale
   var MAX_WIND_INTENSITY = 40;              // wind velocity at which particle intensity is maximum (m/s)
-  var MAX_PARTICLE_AGE = 100;                // max number of frames a particle is drawn before regeneration
-  var PARTICLE_LINE_WIDTH = 2;              // line width of a drawn particle
-  var PARTICLE_MULTIPLIER = 1/30;              // particle count scalar (completely arbitrary--this values looks nice)
+  var MAX_PARTICLE_AGE = 10;                // max number of frames a particle is drawn before regeneration
+  var PARTICLE_LINE_WIDTH = 0.01;              // line width of a drawn particle
+  var PARTICLE_MULTIPLIER = 1/80;             // particle count scalar (completely arbitrary--this values looks nice)
   var PARTICLE_REDUCTION = 0.75;            // reduce particle count to this much of normal for mobile devices
   var FRAME_RATE = 20;                      // desired milliseconds per frame
   var BOUNDARY = 0.45;
